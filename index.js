717a45893c51f3cf42bcd7f2fb2b0747bea20885
John Frades <johnfrades@gmail.com>
2016-11-23 13:50:23 +1300

    hey

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Use body-parser to get POST requests for API use
-app.use(bodyParser.urlencoded({ extended: false }));  
+app.use(bodyParser.urlencoded({ extended: true }));  
 app.use(bodyParser.json());
 
 // Log requests to console
